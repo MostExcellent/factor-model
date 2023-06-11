234435c2e091fbd9717547337d4fcf0e957de68b
MostExcellent <dwglover3@gmail.com>
2023-06-11 16:52:07 -0700

    Removed incorrect market premium.

--- factor-score.py
+++ factor-score.py
@@ -41,32 +41,29 @@ for ticker in tickers:
         security_data = security_data_array.getValueAsElement(i)
         field_data = security_data.getElement('fieldData')
         # print(field_data)
-        last_price = field_data.getElementAsFloat('PX_LAST')
         market_cap = field_data.getElementAsFloat('CUR_MKT_CAP')
         book_value_per_share = field_data.getElementAsFloat('BOOK_VAL_PER_SH')
         roe = field_data.getElementAsFloat('RETURN_COM_EQY')
         free_cash_flow = field_data.getElementAsFloat('CF_FREE_CASH_FLOW')
 
-        data.append([ticker, last_price, market_cap, book_value_per_share, roe, free_cash_flow])
+        data.append([ticker, market_cap, book_value_per_share, roe, free_cash_flow])
 
-df = pd.DataFrame(data, columns=['Ticker', 'LastPrice', 'MarketCap', 'BookValuePerShare', 'ROE', 'FreeCashFlow'])
+df = pd.DataFrame(data, columns=['Ticker', 'MarketCap', 'BookValuePerShare', 'ROE', 'FreeCashFlow'])
 
 # Calculate factors and normalize as you've done previously
-df['MarketPremium'] = df['LastPrice'].pct_change()
 df['Size'] = df['MarketCap']
 df['Value'] = df['BookValuePerShare'] / df['LastPrice']  # Book to market ratio
 df['Profitability'] = df['ROE']
 df['Investment'] = df['FreeCashFlow'] / df['MarketCap']
 
 # Normalize factors to have a common scale
-df['MarketPremiumNorm'] = (df['MarketPremium'] - df['MarketPremium'].mean()) / df['MarketPremium'].std()
 df['SizeNorm'] = (df['Size'] - df['Size'].mean()) / df['Size'].std()
 df['ValueNorm'] = (df['Value'] - df['Value'].mean()) / df['Value'].std()
 df['ProfitabilityNorm'] = (df['Profitability'] - df['Profitability'].mean()) / df['Profitability'].std()
 df['InvestmentNorm'] = (df['Investment'] - df['Investment'].mean()) / df['Investment'].std()
 
 # Calculate score as a sum of all normalized factors (equal weight to all factors)
-df['Score'] = df[['MarketPremiumNorm', 'SizeNorm', 'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']].sum(axis=1)
+df['Score'] = df[['SizeNorm', 'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']].sum(axis=1)
 
 # Sort dataframe by score
 df_sorted = df.sort_values('Score', ascending=False)
