6a8bb1525481f32189c59dd489a471587e878b9e
MostExcellent <dwglover3@gmail.com>
2023-06-14 13:39:06 -0700

    Better hyperparameters using grid search.

--- offline-historic.py
+++ offline-historic.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 
+
 def normalize(x):
-    x = x.fillna(x.mean()) # Fill NaNs with mean
+    x = x.fillna(x.mean())  # Fill NaNs with mean
     if x.std() == 0:
         return x - x.mean()
     else:
@@ -17,16 +18,20 @@ def normalize(x):
         return (x - x.mean()) / x.std()
 
 # Cap and floor outliers
+
+
 def cap_and_floor(df, column, lower_percentile, upper_percentile):
     lower, upper = df[column].quantile([lower_percentile, upper_percentile])
     df[column] = np.where(df[column] < lower, lower, df[column])
     df[column] = np.where(df[column] > upper, upper, df[column])
     return df
 
+
 def process_factors(df):
     print("Processing factors...")
     df_copy = df.copy()
-    df_copy['MarketPremium'] = df_copy['ForwardReturn'] # Will add risk free rate when I get that working
+    # Will add risk free rate when I get that working
+    df_copy['MarketPremium'] = df_copy['ForwardReturn']
     df_copy['Size'] = df_copy['MarketCap']
     df_copy['Value'] = df_copy['BookValuePerShare'] / df_copy['LastPrice']
     df_copy['Profitability'] = df_copy['ROE']
@@ -38,15 +43,44 @@ def process_factors(df):
     df_copy['ProfitabilityNorm'] = normalize(df_copy['Profitability'])
     df_copy['InvestmentNorm'] = normalize(df_copy['Investment'])
 
-    df_copy['Score'] = df_copy[['MarketPremiumNorm', 'SizeNorm', 'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']].sum(axis=1)
+    df_copy['Score'] = df_copy[['MarketPremiumNorm', 'SizeNorm',
+                                'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']].sum(axis=1)
 
     return df_copy
 
+
 def train_model(x_train, y_train):
+
+    # Hyperparameter tuning
+    # Define the parameter grid
+    param_grid = {
+        'n_estimators': [10, 50, 100, 200],
+        'max_depth': [None, 10, 20, 30],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': ['auto', 'sqrt']
+    }
+
+    rf = RandomForestRegressor()
+
+    # Instantiate the grid search model
+    grid_search = GridSearchCV(
+        estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+
+    # Fit the grid search to the data
+    print("Tuning hyperparameters...")
+    grid_search.fit(x_train, y_train)
+
+    # Check the best parameters
+    best_params = grid_search.best_params_
+    print(f"Best parameters: {best_params}")
+
     print("Training model...")
-    rf = RandomForestRegressor(n_estimators=100, random_state=42)
-    rf.fit(x_train, y_train)
-    return rf
+    best_model = RandomForestRegressor(**best_params)
+    best_model.fit(x_train, y_train)
+
+    return best_model
+
 
 def test_model(rf, x_test, y_test):
     print("Testing model...")
@@ -55,12 +89,14 @@ def test_model(rf, x_test, y_test):
     r2 = r2_score(y_test, y_pred)
     return y_pred, mse, r2
 
+
 csv_file = 'data.csv'
 
 # Check if the file exists
 if os.path.isfile(csv_file):
     df = pd.read_csv(csv_file)
-    df = df.dropna(subset=['LastPrice', 'MarketCap', 'BookValuePerShare', 'ROE', 'FreeCashFlow'])
+    df = df.dropna(subset=['LastPrice', 'MarketCap',
+                   'BookValuePerShare', 'ROE', 'FreeCashFlow'])
 else:
     print(f"File {csv_file} not found. Exiting...")
     exit()
@@ -73,24 +109,28 @@ df = cap_and_floor(df, 'FreeCashFlow', 0.01, 0.99)
 
 df['ForwardReturn'] = df.groupby('Ticker')['LastPrice'].pct_change(-1)
 df.dropna(subset=['ForwardReturn'], inplace=True)
-df['ForwardReturnNorm'] = df.groupby('Year')['ForwardReturn'].transform(normalize)
+df['ForwardReturnNorm'] = df.groupby(
+    'Year')['ForwardReturn'].transform(normalize)
 
 #df_grouped = df.groupby(['Ticker', 'Year']).apply(process_factors)
 df_grouped = process_factors(df)
-print(df_grouped[['MarketPremium','ForwardReturn','Size', 'Value', 'Profitability', 'Investment']].isnull().sum())
+print(df_grouped[['MarketPremium', 'ForwardReturn', 'Size',
+      'Value', 'Profitability', 'Investment']].isnull().sum())
 df_grouped.reset_index(inplace=True, drop=True)
 
-features = ['MarketPremiumNorm', 'SizeNorm', 'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']
+# features = ['MarketPremiumNorm', 'SizeNorm', 'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']
+features = ['SizeNorm', 'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']
 target = 'ForwardReturnNorm'
 
 print(df_grouped)
-df_grouped = df_grouped.dropna() # Drop rows with NaN values
+df_grouped = df_grouped.dropna()  # Drop rows with NaN values
 print(df_grouped)
 
 x = df_grouped[features]
 y = df_grouped[target]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42)
 
 rf = train_model(x_train, y_train)
 y_pred, mse, r2 = test_model(rf, x_test, y_test)
@@ -114,11 +154,11 @@ plt.title('Residuals Distribution')
 plt.savefig('residuals.png')
 
 # Distribution of each factor
-for factor in features:
-    plt.figure(figsize=(10, 6))
-    sns.histplot(df_grouped[factor], bins='auto', kde=True)
-    plt.title(f'Distribution of {factor}')
-    plt.savefig(f'{factor}_distribution.png')
+# for factor in features:
+#     plt.figure(figsize=(10, 6))
+#     sns.histplot(df_grouped[factor], bins='auto', kde=True)
+#     plt.title(f'Distribution of {factor}')
+#     plt.savefig(f'{factor}_distribution.png')
 
 # Correlation matrix heatmap
 plt.figure(figsize=(10, 6))
@@ -133,6 +173,9 @@ plt.xlabel('Actual Returns')
 plt.ylabel('Predicted Returns')
 plt.title('Predicted vs. Actual Returns')
 plt.savefig('predicted_vs_actual.png')
+
+# remove later
+exit()
 
 # Bootstrap analysis
 n_samples = 1000
@@ -149,7 +192,8 @@ for _ in range(n_samples):
     y_sample = sample_df[target]
 
     # Split data into train and test sets
-    x_train_sample, x_test_sample, y_train_sample, y_test_sample = train_test_split(x_sample, y_sample, test_size=0.2, random_state=42)
+    x_train_sample, x_test_sample, y_train_sample, y_test_sample = train_test_split(
+        x_sample, y_sample, test_size=0.2, random_state=42)
 
     # Train and test model on the bootstrap sample
     rf = train_model(x_train_sample, y_train_sample)
@@ -168,16 +212,21 @@ feature_importances_transposed = list(map(list, zip(*feature_importances)))
 feature_confidence_intervals = []
 
 for feature_importances in feature_importances_transposed:
-    lower = np.percentile(feature_importances, ((1 - confidence_level) / 2) * 100)
-    upper = np.percentile(feature_importances, (confidence_level + ((1 - confidence_level) / 2)) * 100)
+    lower = np.percentile(feature_importances,
+                          ((1 - confidence_level) / 2) * 100)
+    upper = np.percentile(
+        feature_importances, (confidence_level + ((1 - confidence_level) / 2)) * 100)
     feature_confidence_intervals.append((lower, upper))
 
 residuals_lower = np.percentile(residuals, ((1 - confidence_level) / 2) * 100)
-residuals_upper = np.percentile(residuals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
+residuals_upper = np.percentile(
+    residuals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
 mse_lower = np.percentile(mse_vals, ((1 - confidence_level) / 2) * 100)
-mse_upper = np.percentile(mse_vals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
+mse_upper = np.percentile(
+    mse_vals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
 r2_lower = np.percentile(r2_vals, ((1 - confidence_level) / 2) * 100)
-r2_upper = np.percentile(r2_vals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
+r2_upper = np.percentile(
+    r2_vals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
 
 for i, (lower, upper) in enumerate(feature_confidence_intervals):
     print(f"{confidence_level*100}% confidence interval for feature {i}'s importance: ({lower}, {upper})")
