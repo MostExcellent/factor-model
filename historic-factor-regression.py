08b0ede0ad6645858cbab6c40bce2039ca92eaec
MostExcellent <dwglover3@gmail.com>
2023-06-15 18:42:06 -0700

    gather and process data separately.

--- historic-factor-regression.py
+++ historic-factor-regression.py
@@ -34,7 +34,7 @@ if not session.openService("//blp/refdata"):
 ref_data_service = session.getService("//blp/refdata")
 
 fields = ['PX_LAST', 'CUR_MKT_CAP', 'BOOK_VAL_PER_SH',
-          'RETURN_COM_EQY', 'CF_FREE_CASH_FLOW']  # Bloomberg fields
+          'RETURN_COM_EQY', 'CF_FREE_CASH_FLOW', 'INDUSTRY_SECTOR']  # Bloomberg fields
 
 years = np.arange(START_YEAR, END_YEAR)  # Sample period
 
@@ -155,6 +155,7 @@ def get_data(fields, years, index=INDEX):
                         roe = fetch_field_data(field_data, 'RETURN_COM_EQY')
                         free_cash_flow = fetch_field_data(
                             field_data, 'CF_FREE_CASH_FLOW')
+                        industry_sector = fetch_field_data(field_data, 'INDUSTRY_SECTOR')
 
                         data_rows.append({
                             'Year': year,
@@ -164,6 +165,7 @@ def get_data(fields, years, index=INDEX):
                             'BookValuePerShare': book_value_per_share,
                             'ROE': roe,
                             'FreeCashFlow': free_cash_flow,
+                            'IndustrySector': industry_sector,
                         })
             except Exception as e:
                 print(f"Error for {ticker} in {year}: {e}")
@@ -176,6 +178,7 @@ def get_data(fields, years, index=INDEX):
                     'BookValuePerShare': np.nan,
                     'ROE': np.nan,
                     'FreeCashFlow': np.nan,
+                    'IndustrySector': np.nan,
                 })
 
     df = pd.DataFrame(data_rows)
@@ -226,66 +229,6 @@ def get_risk_free_rate(years=years):
 
     return risk_free_rates
 
-# Noralization helper function
-
-
-def normalize(x):
-    return (x - x.mean()) / x.std()
-
-# Cap and floor outliers
-
-
-def cap_and_floor(df, column, lower_percentile, upper_percentile):
-    lower, upper = df[column].quantile([lower_percentile, upper_percentile])
-    df[column] = np.where(df[column] < lower, lower, df[column])
-    df[column] = np.where(df[column] > upper, upper, df[column])
-    return df
-
-
-def process_factors(df):
-    """
-    Calculate factors and their normalized versions.
-    """
-    print("Processing factors...")
-    df_copy = df.copy()
-    df_copy['MarketPremium'] = df_copy.groupby(
-        'Ticker')['LastPrice'].pct_change() - df_copy['RiskFreeRate']
-    # Size premium is incorrect, and is a plaholder
-    # This is still useful for the regressor, however
-    df_copy['Size'] = df_copy['MarketCap']
-    df_copy['Value'] = df_copy['BookValuePerShare'] / df_copy['LastPrice']
-    df_copy['Profitability'] = df_copy['ROE']
-    df_copy['Investment'] = df_copy['FreeCashFlow'] / df_copy['MarketCap']
-
-    # Normalize factors to have a common scale
-    df_copy['MarketPremiumNorm'] = normalize(df_copy['MarketPremium'])
-    df_copy['SizeNorm'] = normalize(df_copy['Size'])
-    df_copy['ValueNorm'] = normalize(df_copy['Value'])
-    df_copy['ProfitabilityNorm'] = normalize(df_copy['Profitability'])
-    df_copy['InvestmentNorm'] = normalize(df_copy['Investment'])
-
-    # Calculate score as a sum of all normalized factors (equal weight to all factors)
-    df_copy['Score'] = df_copy[['MarketPremiumNorm', 'SizeNorm',
-                                'ValueNorm', 'ProfitabilityNorm', 'InvestmentNorm']].sum(axis=1)
-
-    return df_copy
-
-
-def train_model(x_train, y_train):
-    print("Training model...")
-    rf = RandomForestRegressor(n_estimators=100, random_state=42)
-    rf.fit(x_train, y_train)
-    return rf
-
-
-def test_model(rf, x_test, y_test):
-    print("Testing model...")
-    y_pred = rf.predict(x_test)
-    mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
-    return y_pred, mse, r2
-
-
 # Load data from csv if it exists, else fetch from Bloomberg API
 csv_file = 'data.csv'
 # Check if the file exists
@@ -300,174 +243,17 @@ else:
     # Save to csv to avoid making API calls again in the future
     df.to_csv(csv_file, index=False)
 
-
-# Cap and floor outliers
-df = cap_and_floor(df, 'LastPrice', 0.01, 0.99)
-df = cap_and_floor(df, 'MarketCap', 0.01, 0.99)
-df = cap_and_floor(df, 'BookValuePerShare', 0.01, 0.99)
-df = cap_and_floor(df, 'ROE', 0.01, 0.99)
-df = cap_and_floor(df, 'FreeCashFlow', 0.01, 0.99)
-
-# Validate data types
-print("Validating data types...")
-# print(f"Year is of type {type(df['Year'])}")
-# assert df['Year'].dtype == np.int64, "Year should be an integer"
-# assert df['Ticker'].dtype == str, "Ticker should be a string"
-# assert df['LastPrice'].dtype == np.float64, "LastPrice should be a float"
-# assert df['MarketCap'].dtype == np.float64, "MarketCap should be a float"
-# assert df['BookValuePerShare'].dtype == np.float64, "BookValuePerShare should be a float"
-# assert df['ROE'].dtype == np.float64, "ROE should be a float"
-# assert df['FreeCashFlow'].dtype == np.float64, "FreeCashFlow should be a float"
-
-# Check for duplicates
-assert df.duplicated().sum() == 0, "Data contains duplicated rows"
-
-if df.isnull().any().any():
-    print("Warning: The data contains missing values")
-
 # If risk free rates csv exists, read from it, else fetch from Bloomberg API
 csv_risk_free_rates = 'risk_free_rates.csv'
 
 try:
     print("Reading risk free rates from csv...")
     df_rates = pd.read_csv(csv_risk_free_rates)
-    risk_free_rates = df_rates.groupby('Year')['Rate'].mean().to_dict()
+    risk_free_rates = df_rates.set_index('Year')['Rate'].to_dict()
 except FileNotFoundError:
     print("File not found, fetching risk free rates from Bloomberg API")
     risk_free_rates = get_risk_free_rate()
+    # Save risk_free_rates into a CSV file
+    pd.DataFrame(list(risk_free_rates.items()), columns=['Year', 'Rate']).to_csv(csv_risk_free_rates, index=False)
 
-df['RiskFreeRate'] = df['Year'].map(risk_free_rates)
-
-# Calculate forward returns
-df['ForwardReturn'] = df.groupby('Ticker')['LastPrice'].pct_change(-1)
-# Drop the last row for each ticker as it will have a NaN for ForwardReturn
-df.dropna(subset=['ForwardReturn'], inplace=True)
-# Normalize forward returns
-df['ForwardReturnNorm'] = df.groupby(
-    'Year')['ForwardReturn'].transform(normalize)
-
-# Group by ticker and year and calculate factors
-df_grouped = df.groupby(['Ticker', 'Year']).apply(process_factors)
-df_grouped.reset_index(inplace=True, drop=True)
-
-# Save to csv to avoid making API calls again in the future
-csv_processed = 'processed_data.csv'
-df_grouped.to_csv(csv_processed, index=False)
-# TODO: stick this into a notebook
-
-# Now we do a regression
-
-features = ['MarketPremiumNorm', 'SizeNorm', 'ValueNorm',
-            'ProfitabilityNorm', 'InvestmentNorm']  # ,'Score']
-target = 'ForwardReturnNorm'
-
-x = df_grouped[features]
-y = df_grouped[target]
-
-# Split data into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2, random_state=42)
-
-# Initialize and fit model
-rf = train_model(x_train, y_train)
-
-# Test model
-y_pred, mse, r2 = test_model(rf, x_test, y_test)
-
-print(f'Feature importance: {rf.feature_importances_}')
-print(f'MSE: {mse}')
-print(f'R-squared: {r2}')
-
-# Generate plots
-
-# Feature importance plot
-plt.figure(figsize=(10, 6))
-sns.barplot(x=rf.feature_importances_, y=features)
-plt.title('Feature Importance')
-plt.savefig('feature_importance.png')
-
-# Residuals plot
-residuals = y_test - y_pred
-plt.figure(figsize=(10, 6))
-sns.histplot(residuals, bins='auto', kde=True)
-plt.title('Residuals Distribution')
-plt.savefig('residuals.png')
-
-# Distribution of each factor
-for factor in features:
-    plt.figure(figsize=(10, 6))
-    sns.histplot(df_grouped[factor], bins='auto', kde=True)
-    plt.title(f'Distribution of {factor}')
-    plt.savefig(f'{factor}_distribution.png')
-
-# Correlation matrix heatmap
-plt.figure(figsize=(10, 6))
-sns.heatmap(df_grouped[features].corr(), annot=True, cmap='coolwarm')
-plt.title('Correlation Matrix')
-plt.savefig('correlation_matrix.png')
-
-# Scatter plot of predicted vs. actual returns
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x=y_test, y=y_pred)
-plt.xlabel('Actual Returns')
-plt.ylabel('Predicted Returns')
-plt.title('Predicted vs. Actual Returns')
-plt.savefig('predicted_vs_actual.png')
-
-# Bootstrap analysis
-n_samples = 1000
-
-residuals = []
-feature_importances = []
-mse_vals = []
-r2_vals = []
-
-for _ in range(n_samples):
-    # Bootstrap sample (with replacement)
-    sample_df = df_grouped.sample(frac=1, replace=True, random_state=42)
-    x_sample = sample_df[features]
-    y_sample = sample_df[target]
-
-    # Split data into train and test sets
-    x_train_sample, x_test_sample, y_train_sample, y_test_sample = train_test_split(
-        x_sample, y_sample, test_size=0.2, random_state=42)
-
-    # Train and test model on the bootstrap sample
-    rf = train_model(x_train_sample, y_train_sample)
-    y_pred, mse, r2 = test_model(rf, x_test_sample, y_test_sample)
-
-    # Record the results
-    feature_importances.append(rf.feature_importances_)
-    residuals.append(y_test_sample - y_pred)
-    mse_vals.append(mse)
-    r2_vals.append(r2)
-
-# Now we can calculate the confidence intervals
-confidence_level = 0.95
-
-feature_importances_transposed = list(map(list, zip(*feature_importances)))
-feature_confidence_intervals = []
-
-for feature_importances in feature_importances_transposed:
-    lower = np.percentile(feature_importances,
-                          ((1 - confidence_level) / 2) * 100)
-    upper = np.percentile(
-        feature_importances, (confidence_level + ((1 - confidence_level) / 2)) * 100)
-    feature_confidence_intervals.append((lower, upper))
-
-residuals_lower = np.percentile(residuals, ((1 - confidence_level) / 2) * 100)
-residuals_upper = np.percentile(
-    residuals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
-mse_lower = np.percentile(mse_vals, ((1 - confidence_level) / 2) * 100)
-mse_upper = np.percentile(
-    mse_vals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
-r2_lower = np.percentile(r2_vals, ((1 - confidence_level) / 2) * 100)
-r2_upper = np.percentile(
-    r2_vals, (confidence_level + ((1 - confidence_level) / 2)) * 100)
-
-for i, (lower, upper) in enumerate(feature_confidence_intervals):
-    print(f"{confidence_level*100}% confidence interval for feature {i}'s importance: ({lower}, {upper})")
-
-print(f"{confidence_level*100}% confidence interval for residuals: ({residuals_lower}, {residuals_upper})")
-print(f"{confidence_level*100}% confidence interval for MSE: ({mse_lower}, {mse_upper})")
-print(f"{confidence_level*100}% confidence interval for R^2: ({r2_lower}, {r2_upper})")
+#df['RiskFreeRate'] = df['Year'].map(risk_free_rates)
