e3531a0edb5c87768f4a7dbe7317e9f05e72d8f6
MostExcellent <dwglover3@gmail.com>
2023-06-25 12:42:26 -0700

    fixed thing where it fed in target as feature

--- predict_current.py
+++ predict_current.py
@@ -13,22 +13,45 @@ def normalize(x):
         return (x - x.mean()) / x.std()
 
 
-def process_factors(df):
-    """Process the factors"""
-    df_copy = df.copy()
-    df_copy['Momentum'] = df_copy['LastPrice'] / \
-        df_copy['PreviousYearPrice'] - 1
-    df_copy['Size'] = df_copy['MarketCap']
-    df_copy['Value'] = df_copy['BookValuePerShare'] / df_copy['LastPrice']
-    df_copy['Profitability'] = df_copy['ROE']
-    df_copy['Investment'] = df_copy['FreeCashFlow'] / df_copy['MarketCap']
-    df_copy['MomentumNorm'] = normalize(df_copy['Momentum'])
-    df_copy['SizeNorm'] = normalize(df_copy['Size'])
-    df_copy['ValueNorm'] = normalize(df_copy['Value'])
-    df_copy['ProfitabilityNorm'] = normalize(df_copy['Profitability'])
-    df_copy['InvestmentNorm'] = normalize(df_copy['Investment'])
-    return df_copy
+# def process_factors(df):
+#     """Process the factors"""
+#     df_copy = df.copy()
+#     df_copy['Momentum'] = df_copy['LastPrice'] / \
+#         df_copy['PreviousYearPrice'] - 1
+#     df_copy['Size'] = df_copy['MarketCap']
+#     df_copy['Value'] = df_copy['BookValuePerShare'] / df_copy['LastPrice']
+#     df_copy['Profitability'] = df_copy['ROE']
+#     df_copy['Investment'] = df_copy['FreeCashFlow'] / df_copy['MarketCap']
+#     df_copy['MomentumNorm'] = normalize(df_copy['Momentum'])
+#     df_copy['SizeNorm'] = normalize(df_copy['Size'])
+#     df_copy['ValueNorm'] = normalize(df_copy['Value'])
+#     df_copy['ProfitabilityNorm'] = normalize(df_copy['Profitability'])
+#     df_copy['InvestmentNorm'] = normalize(df_copy['Investment'])
+#     return df_copy
 
+def process_factors(df):
+    """
+    Process factors for a given pandas dataframe by creating new columns for each factor,
+    normalizing each factor within each year, and returning a copy of the dataframe with the new columns.
+    """
+    print("Processing factors...")
+    df_copy = df.copy()
+
+    # shift returns back one year
+    df_copy['Momentum'] = df_copy.groupby(
+        'Ticker')['ForwardReturn'].transform(lambda x: x.shift(1))
+    df_copy['Size'] = df_copy['CUR_MKT_CAP']
+    df_copy['Value'] = df_copy['BOOK_VAL_PER_SH'] / df_copy['PX_LAST']
+    df_copy['ROE'] = df_copy['RETURN_COM_EQY']
+    df_copy['FCF'] = df_copy['CF_FREE_CASH_FLOW'] / df_copy['CUR_MKT_CAP']
+
+    # Normalize within each year
+    for col in ['Momentum', 'Size', 'Value', 'ROE', 'FCF', 'IS_EPS', 'PE_RATIO', 'EPS_GROWTH', 'SALES_GROWTH', 'OPER_MARGIN', 'PROF_MARGIN']:
+        df_copy[f'{col}Norm'] = df_copy.groupby(
+            ['Date'])[col].transform(normalize)
+        
+    df_copy = df_copy.dropna()    
+    return df_copy
 
 csv_file = 'current_data.csv'
 model_file = 'ensemble.pkl'
